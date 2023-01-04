c4871fc09fca112e10c37a6b7db37d43e35e8685
andreipetrut <andreipetrut@outlook.com>
2023-01-04 15:34:18 +0200

    fixed imports, updated endpoints

--- src/main/java/com/goosfraba/cpm/rest/CityRestController.java
+++ src/main/java/com/goosfraba/cpm/rest/CityRestController.java
@@ -1,20 +1,41 @@
 package com.goosfraba.cpm.rest;
 
-import com.goosfraba.cpm.dao.CityDAO;
 import com.goosfraba.cpm.entity.City;
+import com.goosfraba.cpm.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CityRestController {
+    private CityService cityService;
+
     @Autowired
-    private CityDAO cityDAO;
+    public CityRestController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    @GetMapping("/cities")
+    public List<City> findAll() {
+        return cityService.findAll();
+    }
+
+    @GetMapping("/cities/{cityId}")
+    public City findById(@PathVariable int cityId) {
+        return cityService.findById(cityId);
+    }
+
+    @GetMapping("/cities/code/{cityCode}")
+    public City findByCode(@PathVariable String cityCode) {
+        return cityService.findByCode(cityCode);
+    }
 
     @PostMapping("/cities")
     public City addCity(@RequestBody City city) {
         city.setId(String.valueOf(0));
-        cityDAO.save(city);
+        cityService.save(city);
         return city;
     }
+
 }
